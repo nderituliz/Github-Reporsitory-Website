6a68cf65a474e06b28b63c3ae40155f3393aaf60
Elizabeth Nderitu <nderituliz@gmail.com>
2020-11-15 20:42:45 +0300

    Add github services that fetches the profile imformation and updates the userprofile

---
+++ src/app/github/github.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../services/github.service';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+
+profile:any;
+repos: any;
+username: string;
+
+  constructor(private githubService:GithubService) {
+    this.githubService.updateUserProfile(this.username);
+    this.githubService.getProfileInformation().subscribe(profile => {
+      this.profile = profile;
+    });
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
